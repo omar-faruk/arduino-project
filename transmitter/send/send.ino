689d7fcbcf7d7c0510d1ea94493a71af9da45355
omar-faruk <munna93_bd@yahoo.com>
2015-01-22 20:52:59 +0600

    upload

--- transmitter/send/send.ino
+++ transmitter/send/send.ino
@@ -1,24 +1,28 @@
 int time=0,i;
+
 void setup() {
   pinMode(6,OUTPUT);
+  pinMode(8,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop(){
-  i=0;
+ int i=0;
+ String s;
   if(Serial.available()) {
     String s=Serial.readString();
-    Serial.println(s);
+    //Serial.println(s);
+  int len=s.length();
+  for(i=1;i<len-1;i++){
+      Serial.println(int(s[i]-'0'));
+      digitalWrite(6,int(s[i]-'0'));
+      digitalWrite(8,int(s[i+1]-'0'));
+      delay(50);
   }
-  for(i=1;i<=time;i++){
-    digitalWrite(6,1);
-    delay(150);
-    digitalWrite(6,0);
-    delay(100);
   }
-  time=0;
+  
+  
 }
-
 
 
 
