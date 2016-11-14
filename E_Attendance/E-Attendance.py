32c67cf308ae0091ff5b000b7b8d06f375d4acc1
Omar Faruk <munna93_bd@yahoo.com>
2016-11-15 03:36:27 +0600

    data entry to MySQL db & UI changed

--- E_Attendance/E-Attendance.py
+++ E_Attendance/E-Attendance.py
@@ -1,10 +1,11 @@
 from gi.repository import Gtk, GObject
+import MySQLdb as my
 import serial
 import datetime
 from time import sleep
 import os
 
-class MessengerGUI:
+class GUI:
 	arduino=None
 		
 	def __init__(self):
@@ -15,14 +16,23 @@ class MessengerGUI:
 		self.dump_button=builder.get_object("dump")
 		self.registration_button=builder.get_object("registration")
 		self.coursereg_button=builder.get_object("coursereg")
+		
 		self.entry_box=builder.get_object("entry_box")
+		self.entry_box1=builder.get_object("entry_box1")
+		
 		self.date_button=builder.get_object("date")
-		self.clear_button=builder.get_object("cleardb")
+		self.count_button=builder.get_object("countatd")
+		
 		self.showBox=builder.get_object("receive_box")
+		self.showBox2=builder.get_object("receive_box2")
+		self.showBox3=builder.get_object("receive_box3")
+		
 		self.connect_button=builder.get_object("connect")
 		
 		builder.connect_signals(self)
-	
+
+
+
 	def on_registration_clicked(self,button):
 		global arduino
 		try:
@@ -30,7 +40,9 @@ class MessengerGUI:
 			self.showBox.set_text("Device set to Registration Mode")
 		except:
 			self.showBox.set_text("Failed to set Registration Mode")
+
 			
+
 	def on_coursereg_clicked(self,buttton):
 		#edit the contents here
 		global arduino
@@ -42,7 +54,9 @@ class MessengerGUI:
 			self.showBox.set_text("Course Code Initialized, ReStart Device")
 		except:
 			self.showBox.set_text("Failed to initialize Courses, Retry")
+
 	
+
 	def on_date_clicked(self,button):
 		global arduino
 		try:
@@ -51,18 +65,25 @@ class MessengerGUI:
 			formated_date="dt"+date
 			arduino.write(formated_date)
 			print(formated_date)
-			self.showBox.set_text("Date Updated")
+			self.showBox3.set_text("Date Updated")
 		except:
-			self.showBox.set_text("Failed to set Date")
-	
-	def on_cleardb_clicked(self,button):
-		global arduino
-		try:
-			arduino.write("cleardb")
-			self.showBox.set_text("SD database cleared")
-		except:
-			self.showBox.set_text("Failed to clear SD database")
-	
+			self.showBox3.set_text("Failed to set Date")
+
+
+
+	def on_countatd_clicked(self,button):
+		print ('attendance counting')
+		courses=self.entry_box1.get_text();
+		course_list=courses.split()
+		success=0
+		for course_code in course_list:
+			#print (course_code)
+			count_atd(course_code)
+			self.entry_box1.set_placeholder_text("done")
+			
+
+
+
 	def on_connect_clicked(self,button):
 		connect_arduino=False
 		deviceList=['/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2','/dev/ttyACM3']
@@ -76,6 +97,8 @@ class MessengerGUI:
 			if(connect_arduino==True):
 				self.showBox.set_text("Connection Successfull!!!")
 				break
+
+
 
 	def on_dump_clicked(self,button):
 		#edit the contents here
@@ -92,17 +115,22 @@ class MessengerGUI:
 					print sms
 					count=count+1
 			if(count<1):
-					self.showBox.set_text("Data File may not exists,\n check device display? ")
+					self.showBox2.set_text("Data File may not exists,\n check device display? ")
 			else:
 				f.close()
-				self.showBox.set_text("Reading Complete")
+				self.showBox2.set_text("Reading Complete")
 		except:
-			self.showBox.set_text("Error !!! Read Failed")
+			self.showBox2.set_text("Error !!! Read Failed")
 		f.close()
 		processFile()
 		
+
 	def mnu_quite_app(self, window):
 		Gtk.main_quit()
+#class definition ends here
+
+
+
 
 def processFile():
 	tempFile=open('temp.txt','r')
@@ -120,10 +148,63 @@ def processFile():
 			if(c!='#' and c!="\n"):
 				courseFile.write(c)
 	courseFile.close()
+
+
+
+def count_atd(course_code):
+	with open(course_code,'r+') as text:
+		while(True):
+			line=text.readline()
+			if not line:
+				break
+			line=line.replace(',',' ')
+			raw_list=line.split(' ')
+			
+			student_id=[]
+			atd_count=[]
+			counter=0
+			date_count=0
+			
+			for i in range (0,len(raw_list)):
+				if(len(raw_list[i])>8):
+					student_id.append(raw_list[i])
+					if(counter>0):
+						atd_count.append(date_count)
+						date_count=0
+					counter=counter+1
+				elif(len(raw_list[i])==8):
+					date_count=date_count+1
+				if(i==len(raw_list)-1):
+					atd_count.append(date_count)
+			
+			atd_count_file=course_code+"_final.txt"
+			course_atd_file=open(atd_count_file,'a+')
+			for i in range (0,len(student_id)):
+				if(i==0):
+					student_id[0]=student_id[0][1:]
+				course_atd_file.write(student_id[i]+" "+str(atd_count[i])+"\n")
+				insert_into_db(student_id[i],course_code,atd_count[i])
+				#print(student_id[i])
+				#print(atd_count[i])
+				#print("\n")
+			course_atd_file.close()
+		text.close()
+
+
+def insert_into_db(id_card,course_code,atd_count):
+	db = my.connect(host="127.0.0.1",user="root",passwd="cse22@JU",db="test")
+ 
+	cursor = db.cursor()
+	
+	sql = "insert into attendance VALUES('%s', '%s', %d)" % (id_card,course_code,atd_count)
+	
+	number_of_rows = cursor.execute(sql)
+	db.commit()   # you need to call commit() method to save 
+	db.close()
 	
 	
 if __name__ == '__main__':
-    messenger = MessengerGUI()
+    messenger = GUI()
     messenger.window.connect("delete-event", Gtk.main_quit)
     messenger.window.show_all()
     Gtk.main()
