1d5397eb5c39fa7ec846b8da60593b9c47aeace3
Omar Faruk <munna93_bd@yahoo.com>
2016-11-15 00:39:25 +0600

    New UI and code revised

--- E_Attendance/E-Attendance.py
+++ E_Attendance/E-Attendance.py
@@ -1,5 +1,6 @@
 from gi.repository import Gtk, GObject
 import serial
+import datetime
 from time import sleep
 import os
 
@@ -8,15 +9,18 @@ class MessengerGUI:
 		
 	def __init__(self):
 		builder = Gtk.Builder()
-		builder.add_from_file("messenger.glade")
-		self.window = builder.get_object("messengerWindow")
+		builder.add_from_file("gui.glade")
+		self.window = builder.get_object("mainWindow")
+		
 		self.dump_button=builder.get_object("dump")
 		self.registration_button=builder.get_object("registration")
-		self.deregistration_button=builder.get_object("deregistration")
-		self.dump_button=builder.get_object("attendance")
-		self.dump_button=builder.get_object("cleardb")
+		self.coursereg_button=builder.get_object("coursereg")
+		self.entry_box=builder.get_object("entry_box")
+		self.date_button=builder.get_object("date")
+		self.clear_button=builder.get_object("cleardb")
 		self.showBox=builder.get_object("receive_box")
 		self.connect_button=builder.get_object("connect")
+		
 		builder.connect_signals(self)
 	
 	def on_registration_clicked(self,button):
@@ -27,21 +31,29 @@ class MessengerGUI:
 		except:
 			self.showBox.set_text("Failed to set Registration Mode")
 			
-	def on_deregistration_clicked(self,buttton):
+	def on_coursereg_clicked(self,buttton):
+		#edit the contents here
 		global arduino
+		courses=self.entry_box.get_text()
+		courses='cr'+courses+'#'
+		print courses
 		try:
-			arduino.write("deregistration-mode")
-			self.showBox.set_text("Device set to De-Registration Mode")
+			arduino.write(courses)
+			self.showBox.set_text("Course Code Initialized, ReStart Device")
 		except:
-			self.showBox.set_text("Failed to set De-Registration Mode")
+			self.showBox.set_text("Failed to initialize Courses, Retry")
 	
-	def on_attendance_clicked(self,button):
+	def on_date_clicked(self,button):
 		global arduino
 		try:
-			arduino.write("attendance-mode")
-			self.showBox.set_text("Device set to Attendance Mode")
+			date=str(datetime.date.today())
+			date=date.replace('-','')
+			formated_date="dt"+date
+			arduino.write(formated_date)
+			print(formated_date)
+			self.showBox.set_text("Date Updated")
 		except:
-			self.showBox.set_text("Failed to set Attendance Mode")
+			self.showBox.set_text("Failed to set Date")
 	
 	def on_cleardb_clicked(self,button):
 		global arduino
@@ -66,11 +78,12 @@ class MessengerGUI:
 				break
 
 	def on_dump_clicked(self,button):
+		#edit the contents here
 		global arduino
 		count=0
-		f=open('db.txt','w')
+		f=open('temp.txt','w')
 		try:
-			arduino.write("dump-atd")
+			arduino.write("dump")
 			sleep(2.5)
 			while(arduino.inWaiting()>=2):
 				sms=arduino.readline()
@@ -82,13 +95,33 @@ class MessengerGUI:
 					self.showBox.set_text("Data File may not exists,\n check device display? ")
 			else:
 				f.close()
-				self.showBox.set_text("Dumping Complete")
+				self.showBox.set_text("Reading Complete")
 		except:
-			self.showBox.set_text("Error !!! Dumping Failed")
-			
+			self.showBox.set_text("Error !!! Read Failed")
+		f.close()
+		processFile()
+		
 	def mnu_quite_app(self, window):
 		Gtk.main_quit()
 
+def processFile():
+	tempFile=open('temp.txt','r')
+	courseName=""
+	flag=0;
+	while(True):
+		c=tempFile.read(1)
+		if not c:
+			break
+		if(c!='#' and flag==0):
+			courseName=courseName+c;
+		else:
+			flag=1
+			courseFile=open(courseName,'a')
+			if(c!='#' and c!="\n"):
+				courseFile.write(c)
+	courseFile.close()
+	
+	
 if __name__ == '__main__':
     messenger = MessengerGUI()
     messenger.window.connect("delete-event", Gtk.main_quit)
--- E_Attendance/string_process.h
+++ E_Attendance/string_process.h
@@ -1,6 +1,5 @@
-char c;
 String tag = "", command,mode,date="";
-String course[5];
+String course[6];
 
 void processTag(){
   tag+='c';
@@ -11,5 +10,23 @@ void processTag(){
   tag[8]='.';
 }
 void toChar(char result[],String value){
-  value.toCharArray(result,13);
+  int i,n=value.length();
+  for(int i=0;i<n;i++){
+	  result[i]=value[i];
+  }
+  result[n]=0;
 } 
+
+boolean isNum(char c){
+	if(c=='0') return 1;
+	if(c=='1') return 1;
+	if(c=='2') return 1;
+	if(c=='3') return 1;
+	if(c=='4') return 1;
+	if(c=='5') return 1;
+	if(c=='6') return 1;
+	if(c=='7') return 1;
+	if(c=='8') return 1;
+	if(c=='9') return 1;
+	else return 0;
+}
